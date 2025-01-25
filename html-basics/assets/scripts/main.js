62819104c80bcfa148b6c8b22bff92bc1e32fbe4
oleksharh <oleksharh@gmail.com>
2025-01-25 22:11:02 +0000

    Refactored main.css
    refactored out the css from a single file into multiple files, allowing easier scalability

--- html-basics/assets/scripts/main.js
+++ html-basics/assets/scripts/main.js
@@ -1,3 +1,15 @@
 function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-  }
+  document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+// Theme toggle
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.textContent = currentTheme === 'dark' ? 'LIGHT MODE' : 'DARK MODE';
+
+toggleButton.addEventListener('click', () => {
+  let newTheme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
+  toggleButton.textContent = newTheme === 'dark' ? 'LIGHT MODE' : 'DARK MODE';
+
+  localStorage.setItem('theme', newTheme);
+});
